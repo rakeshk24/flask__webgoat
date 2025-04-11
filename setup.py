2fd93a3864ca68d78790659dadf283d7ce626135
Rakesh <rakeshk.24@gmail.com>
2025-04-11 13:20:36 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flask_webgoat_app',
+      version='0.1',
+      description='sample python app',
+      author='JFrog',
+      license='MIT',
+      packages=['flask-webgoat_app'],
+      install_requires=[
+          'PyYAML==5.2',
+      ])
